edb2782fb2e8a4a4b900914448a9d54817745c80
thayllaa <thaylla.santana@fatec.sp.gov.br>
2022-03-18 20:10:37 -0300

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { HomeComponent } from './home/home.component';
+import { JurosComponent } from './juros/juros.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, HelloComponent, HomeComponent, JurosComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
