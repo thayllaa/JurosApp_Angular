c3f56b3bab935269f95f5c071b6afabb6e98e7c4
thayllaa <thaylla.santana@fatec.sp.gov.br>
2022-03-18 20:01:36 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-w43pag
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-w43pag)
---
+++ src/app/juros/juros.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-juros',
+  templateUrl: './juros.component.html',
+  styleUrls: ['./juros.component.css']
+})
+export class JurosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
